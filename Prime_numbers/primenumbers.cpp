80cc0a68b9ce53d30f6a6779f7c0b84c127499d5
raoalinawaz-cys <97831313+raoalinawaz-cys@users.noreply.github.com>
2022-03-29 08:34:15 +0500

    Update primenumbers.cpp

--- Prime_numbers/primenumbers.cpp
+++ Prime_numbers/primenumbers.cpp
@@ -1,30 +1,39 @@
 // Write a program to check whether a number is prime or not
 //Prime number are the numbers which divide only by 1 and itself
 //By RAO ALI NAWAZ
-#include <iostream>
-using namespace std;
+
+#include <iostream> 
+using namespace std; 
 int main()
 {
 	int number;
-	cout << "Enter any number: ";
-	cin >> number;//storing user input number into number variable
-	if(number == 1)//making condition for 1
+	cout << "Enter a number: ";
+	cin >> number;
+	if(number == 0 || number == 1) 
 	{
-		cout << number << " is not prime.";//1 is not prime
+   		cout << number << " is not a prime number." << endl;
 	}
-  //if number is not properly divided by 2 and 3 and give some remainder other than 0 then number is prime
-	else if(number % 2 && number % 3)
-	{
-		cout << number << " is prime.";
-	}
-	// Making conditions for 2 and 3 prime numbers
 	else if(number == 2 || number == 3)
 	{
-		cout << number << " is prime.";
+		cout << number << " is a prime number." << endl;
 	}
 	else
 	{
-		cout << number << " is not prime.";
+		int check = 0;
+        for(int i = 2; i <= number/2; i++)
+        {
+            if(number % i == 0)
+            {
+            	cout << number << " is not a prime number." << endl;
+                check = 1;
+                break;
+            }
+        }
+        if (check == 0)
+        {
+        	cout << number << " is a prime number." << endl;
+		}
 	}
 	return 0;
 }
+
