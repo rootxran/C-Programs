4123d2a28f819e95f9fb39332a4e504aa4370103
raoalinawaz-cys <97831313+raoalinawaz-cys@users.noreply.github.com>
2022-03-29 08:54:48 +0500

    Create Pyramidofstars.cpp

---
+++ Stars by loops/Pyramidofstars.cpp
@@ -0,0 +1,27 @@
+//Write a program to print pyramid of stars by using loops
+//By RAO ALI NAWAZ
+#include <iostream>
+using namespace std;
+int main()
+{
+//      *
+//     ***
+//    *****
+//   *******
+	int n = 4;
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= 2 * n - 1; j++)
+		{
+			if(j >= n -(i - 1) && j <= n + (i - 1))
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+}
