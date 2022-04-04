e40c167170a48e6d5f8c3d6d12b7509d9a31feb0
raoalinawaz-cys <97831313+raoalinawaz-cys@users.noreply.github.com>
2022-04-04 23:32:22 +0500

    Update printtableofnumberusingwhileloop.cpp

--- Print table of number using while loop/printtableofnumberusingwhileloop.cpp
+++ Print table of number using while loop/printtableofnumberusingwhileloop.cpp
@@ -1,16 +1,16 @@
-//Write a program to take number from user and print table of that number using while loop
+//Write a program to take number from user and print table of that number using do-while loop
 //By RAO ALI NAWAZ
-include <iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 	int i = 1, number;//declaring variables and initializing variable i to 1 for loop
 	cout << "Enter a number: ";
 	cin >> number;
-	while(i <= 10)
+	do
 	{
 		cout << number << " * " << i << " = " << number * i << endl;
 		i++;//incrementing i to avoid infinite loop
-	}
-  return 0;
+	}while(i <= 10);
+  	return 0;
 }
