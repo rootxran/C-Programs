3e630405eb99e68fd9195f5c1a0da0bf3c9a2b98
raoalinawaz-cys <97831313+raoalinawaz-cys@users.noreply.github.com>
2022-05-28 14:01:14 +0500

    Update sortelementsinascendinganddescendingorderusingbubblesortalgorithm.cpp

--- Arrays/sortelementsinascendinganddescendingorderusingbubblesortalgorithm.cpp
+++ Arrays/sortelementsinascendinganddescendingorderusingbubblesortalgorithm.cpp
@@ -30,15 +30,15 @@ void bubblesort(int a[], int choice, int size)
 }
     if(choice == 2)
     {
-        for(int i = size - 1; i >= 0; i--)
+        for(int i = 0; i < size; i++)
         {
-            for(int j = size - 1; j > (size - 1) - i; j--)
+            for(int j = 0; j < size - i; j++)
             {
-                if(a[j] >  a[j - 1])
+                if(a[j] < a[j + 1])
                 {
                     temp = a[j];
-                    a[j] = a[j - 1];
-                    a[j - 1] = temp;
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
